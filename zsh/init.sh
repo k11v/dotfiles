ea042eb1682024d25f16756e0f912f092a099095
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-10-05 23:59:33 +0300

    Clean zsh

--- zsh/init.sh
+++ zsh/init.sh
@@ -1,3 +1,13 @@
+export FPATH="$XDG_CONFIG_HOME/zsh/completions:$FPATH"
+export HISTFILE="$XDG_DATA_HOME/zsh/.zhistory"
+export HISTSIZE=10000
+export KEYTIMEOUT=1
+export PROMPT_EOL_MARK=""
+export PROMPT_STYLE="regular"                     # User-defined
+export PS2="%B…%b "
+export SAVEHIST=10000
+export ZCOMPDUMP="$XDG_CACHE_HOME/zsh/.zcompdump" # User-defined
+
 if [ "$INSTALL" -eq 1 ]; then
     install-file "$HOME/.zshenv" .zshenv
     install-file "$XDG_CONFIG_HOME/zsh" config
