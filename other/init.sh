b02821cc58f7645143e8adae72afb032ba10b1e1
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-10-13 23:41:12 +0300

    Add other

---
+++ other/init.sh
@@ -0,0 +1,113 @@
+if [ "$INSTALL" -eq 1 ]; then
+    brew install binutils
+    brew install findutils
+    brew install gawk
+    brew install gnu-getopt
+    brew install gnu-indent
+    brew install gnu-sed
+    brew install gnu-tar
+    brew install gnutls
+    brew install inetutils
+    brew install make
+    brew install moreutils
+
+    brew install diff-so-fancy
+    brew install duf
+    brew install duti
+    brew install fd
+    brew install ffmpeg
+    brew install fzf
+    brew install gh
+    brew install gnupg
+    brew install graphviz
+    brew install grip
+    brew install htop
+    brew install httpie
+    brew install imagemagick
+    brew install jq
+    brew install librsvg
+    brew install mas
+    brew install ncdu
+    brew install nload
+    brew install p7zip
+    brew install pandoc
+    brew install pipx
+    brew install ripgrep
+    brew install shadowsocks-rust
+    brew install speedtest-cli
+    brew install tree
+    brew install v2ray
+    brew install websocat
+    brew install wget
+    brew install wireguard-tools
+    brew install youtube-dl
+
+    brew install gcc
+    brew install lua
+    brew install node
+    brew install openjdk
+    brew install rust
+
+    brew install ansible
+    brew install clang-format
+    brew install golang-migrate
+    brew install google-java-format
+    brew install libpq
+    brew install mongodb/brew/mongodb-database-tools
+    brew install mysql-client
+    brew install poetry
+    brew install stylua
+    brew install yarn
+
+    brew install binwalk
+    brew install bulk_extractor
+    brew install exiftool
+    brew install fcrackzip
+    brew install gobuster
+    brew install hydra
+    brew install john-jumbo
+    brew install masscan
+    brew install massdns
+    brew install mitmproxy
+    brew install nmap
+    brew install owasp-amass/amass/amass
+    brew install rustscan
+    brew install sleuthkit
+    brew install sqlmap
+    brew install sslsplit
+    brew install subfinder
+    brew install virustotal-cli
+    brew install volatility
+    brew install yara
+
+    brew install --cask alfred
+    brew install --cask appcleaner
+    brew install --cask discord
+    brew install --cask docker
+    brew install --cask figma
+    brew install --cask firefox
+    brew install --cask font-xits
+    brew install --cask gimp
+    brew install --cask google-chrome
+    brew install --cask imhex
+    brew install --cask mactex-no-gui
+    brew install --cask ngrok
+    brew install --cask notion
+    brew install --cask openvpn-connect
+    brew install --cask postman
+    brew install --cask rar
+    brew install --cask shadowsocksx-ng
+    brew install --cask spotify
+    brew install --cask tor-browser
+    brew install --cask tunnelblick
+    brew install --cask visual-studio-code
+    brew install --cask vlc
+    brew install --cask vmware-fusion
+    brew install --cask wireshark
+
+    mas install 1352778147 # Bitwarden
+    mas install 1487937127 # Craft
+    mas install 1356178125 # Outline
+    mas install 904280696  # Things
+    mas install 1451685025 # WireGuard
+fi
