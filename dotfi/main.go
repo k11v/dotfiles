afd6379d9e0d593de7fdccc86d495b3edff6111d
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-10-27 18:10:20 +0300

    Add flag parsing to dotfi

--- dotfi/main.go
+++ dotfi/main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
+var errFlagParse = errors.New("failed flag parse")
+
 func main() {
-	if err := run(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	if err := run(os.Args, os.Stdout, os.Stderr); err != nil {
+		if !errors.Is(err, errFlagParse) {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		}
 		os.Exit(1)
 	}
 	os.Exit(0)
 }
 
-func run() error {
+func run(args []string, stdout io.Writer, stderr io.Writer) error {
+	fs := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	fs.SetOutput(stderr)
+
+	if err := fs.Parse(args[1:]); err != nil {
+		if errors.Is(err, flag.ErrHelp) {
+			return nil
+		}
+		return errors.Join(errFlagParse, err)
+	}
+
 	return nil
 }
