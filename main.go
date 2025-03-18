49785fc560e6cc0df2c7a62a71536c1c3e4cd61c
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-03-18 21:18:36 +0300

    Add Brewer, Enver, Filer, Runner

--- main.go
+++ main.go
@@ -1,4 +1,30 @@
 package main
 
+import "os/exec"
+
+type Brewer struct{}
+
+func (b *Brewer) Brew(p string) error {
+	return nil
+}
+
+type Enver struct{}
+
+func (e *Enver) Env(k, v string) error {
+	return nil
+}
+
+type Filer struct{}
+
+func (f *Filer) File(dst, src string) error {
+	return nil
+}
+
+type Runner struct{}
+
+func (e *Runner) Run(c *exec.Cmd) error {
+	return nil
+}
+
 func main() {
 }
