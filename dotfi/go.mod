306aa677de1a0f17367d318c3bf6eb15c1d752a9
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-03-19 14:39:12 +0300

    Add dotfi module parsing

--- dotfi/go.mod
+++ dotfi/go.mod
@@ -1,3 +1,5 @@
 module github.com/k11v/dotfiles/dotfi
 
 go 1.24.1
+
+require github.com/pelletier/go-toml/v2 v2.2.3
--- dotfi/main.go
+++ dotfi/main.go
@@ -1,4 +1,55 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
+type Module struct {
+	Alias       [][]any
+	BrewCask    [][]any
+	BrewFormula [][]any
+	Default     [][]any
+	Env         [][]any
+	File        [][]any
+	Mas         [][]any
+	Run         [][]any
+	RunSudo     [][]any
+}
+
 func main() {
+	flag.Parse()
+
+	moduleFiles := flag.Args()
+	modules := make(map[string]*Module, len(moduleFiles))
+
+	for _, moduleFile := range moduleFiles {
+		err := func() error {
+			modules[moduleFile] = new(Module)
+
+			f, err := os.Open(moduleFile)
+			if err != nil {
+				return err
+			}
+
+			dec := toml.NewDecoder(f)
+			dec.DisallowUnknownFields()
+			err = dec.Decode(modules[moduleFile])
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	for name, module := range modules {
+		fmt.Printf("%q = %#v\n", name, module)
+	}
 }
