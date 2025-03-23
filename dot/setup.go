86426396b262f459b2bb6b2a998ca2424b1b4b98
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-03-24 00:28:20 +0300

    Add dot setup notes

--- dot/setup.go
+++ dot/setup.go
@@ -13,22 +13,38 @@ import (
 )
 
 // TODO: Add variable expansion (e.g. "$XDG_CONFIG_HOME").
+// TODO: Add idempotency.
+// TODO: Remove stdin usage.
 
 type Setupper struct {
 	envFile string
 }
 
+// "brew install --cask -- alacritty" is idempotent.
+// "..." automatically updates Homebrew.
+// "..." upgrades cask if already installed.
+// "..." likely automatically cleans up.
+// "..." might ask for password.
+// TODO: Study ansible, nix and brew bundle.
 func (s *Setupper) SetupBrewCask(d BrewCask) error {
 	c := exec.Command("brew", "install", "--cask", "--", d.Name)
-	c.Stdin = os.Stdin // TODO: Change to not use stdin.
+	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	return c.Run()
 }
 
+// "brew install --formula -- tmux" is idempotent.
+// "..." automatically updates Homebrew.
+// "..." upgrades formula if already installed.
+// "..." builds formula if bottle is unavailable.
+// "..." likely automatically cleans up.
+// "..." might not link executables, e.g. due conflicts.
+// "..." might ask for password.
+// TODO: Study ansible, nix and brew bundle.
 func (s *Setupper) SetupBrewFormula(d BrewFormula) error {
 	c := exec.Command("brew", "install", "--formula", "--", d.Name)
-	c.Stdin = os.Stdin // TODO: Change to not use stdin.
+	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	return c.Run()
@@ -68,12 +84,13 @@ func (s *Setupper) SetupDefault(d Default) error {
 	}
 
 	c := exec.Command(args[0], args[1:]...)
-	c.Stdin = os.Stdin // TODO: Change to not use stdin.
+	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	return c.Run()
 }
 
+// TODO: Change SetupEnvAlias to be idempotent.
 func (s *Setupper) SetupEnvAlias(d EnvAlias) error {
 	err := os.MkdirAll(filepath.Dir(s.envFile), 0o777)
 	if err != nil {
@@ -97,6 +114,7 @@ func (s *Setupper) SetupEnvAlias(d EnvAlias) error {
 	return err
 }
 
+// TODO: Change SetupEnvAlias to be idempotent.
 func (s *Setupper) SetupEnvVar(d EnvVar) error {
 	err := os.MkdirAll(filepath.Dir(s.envFile), 0o777)
 	if err != nil {
@@ -120,6 +138,7 @@ func (s *Setupper) SetupEnvVar(d EnvVar) error {
 	return err
 }
 
+// TODO: Change SetupEnvAlias to be idempotent.
 func (s *Setupper) SetupLink(d Link) error {
 	err := os.MkdirAll(filepath.Dir(d.Dst), 0o777)
 	if err != nil {
@@ -128,9 +147,13 @@ func (s *Setupper) SetupLink(d Link) error {
 	return os.Symlink(d.Src, d.Dst)
 }
 
+// "mas install -- 904280696" is idempotent.
+// "..." likely open App Store for password the first time.
+// TODO: Consider supporting upgrades.
+// TODO: Study ansible, nix and brew bundle.
 func (s *Setupper) SetupMas(d Mas) error {
 	c := exec.Command("mas", "install", "--", d.ID)
-	c.Stdin = os.Stdin // TODO: Change to not use stdin.
+	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	return c.Run()
@@ -138,7 +161,7 @@ func (s *Setupper) SetupMas(d Mas) error {
 
 func (s *Setupper) SetupRun(d Run) error {
 	c := exec.Command("sh", "-c", d.Command)
-	c.Stdin = os.Stdin // TODO: Change to not use stdin.
+	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	return c.Run()
