f52e20274bef2b8425cfcd3cf896f3d98e8cbd5d
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-03-24 16:31:14 +0300

    Tidy

--- dot/main.go
+++ dot/main.go
@@ -212,8 +212,7 @@ func LoadMods(mods []string) (map[string][]Decl, error) {
 			return nil
 		}()
 		if err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
-			os.Exit(1)
+			return nil, err
 		}
 	}
 
