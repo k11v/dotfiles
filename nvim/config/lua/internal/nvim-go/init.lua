e2eae8697fd59a3847bb60be76a6be9a56a92b06
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2025-09-28 05:57:00 +0300

    Add nvim go gotmpl filetype

--- nvim/config/lua/internal/nvim-go/init.lua
+++ nvim/config/lua/internal/nvim-go/init.lua
@@ -1,16 +1,16 @@
 return {
 	setup = function()
-		require("internal.nvim").setup_filetypes({ "go", "gomod", "gowork", "gosum" }, function()
+		require("internal.nvim").setup_filetypes({ "go", "gomod", "gowork", "gosum", "gotmpl" }, function()
 			vim.opt.colorcolumn = { "120" }
 			vim.opt.expandtab = false
 			vim.opt.shiftwidth = 4
 			vim.opt.softtabstop = 4
 			vim.opt.tabstop = 4
 		end)
-		require("internal.nvim").treesitter_parsers({ "go", "gomod", "gowork", "gosum" })
-		require("internal.nvim").treesitter_highlighting_filetypes({ "go", "gomod", "gowork", "gosum" })
-		require("internal.nvim").treesitter_folding_filetypes({ "go", "gomod", "gowork", "gosum" })
-		require("internal.nvim").treesitter_indenting_filetypes({ "go", "gomod", "gowork", "gosum" })
+		require("internal.nvim").treesitter_parsers({ "go", "gomod", "gowork", "gosum", "gotmpl" })
+		require("internal.nvim").treesitter_highlighting_filetypes({ "go", "gomod", "gowork", "gosum", "gotmpl" })
+		require("internal.nvim").treesitter_folding_filetypes({ "go", "gomod", "gowork", "gosum", "gotmpl" })
+		require("internal.nvim").treesitter_indenting_filetypes({ "go", "gomod", "gowork", "gosum", "gotmpl" })
 		require("internal.nvim").lsp_servers({ "gopls" })
 		require("internal.nvim").executables({ "gopls" })
 	end,
