1f286c621996dc5d4170232f6474228c53a507c4
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2025-10-26 02:45:40 +0300

    Change nvim setup to contain everything

--- nvim/config/init.lua
+++ nvim/config/init.lua
@@ -1,27 +1,3 @@
--- Setup mini.deps.
-
-local deps_path = vim.fn.stdpath("data") .. "/site/pack/deps/start/mini.deps"
-
-if not vim.loop.fs_stat(deps_path) then
-	vim.cmd('echo "Installing `mini.deps`" | redraw')
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/nvim-mini/mini.deps",
-		deps_path,
-	})
-	vim.cmd("packadd mini.deps | helptags ALL")
-	vim.cmd('echo "Installed `mini.deps`" | redraw')
-end
-
-require("mini.deps").setup()
-
--- Setup nvim.
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
 require("internal.nvim").setup({
 	vim_setup_arms = {
 		{
--- nvim/config/lua/internal/nvim/init.lua
+++ nvim/config/lua/internal/nvim/init.lua
@@ -34,6 +34,9 @@ M.setup = function(opts)
 
 	local vim_setup_arms = (opts or {}).vim_setup_arms or {}
 
+	vim.g.mapleader = " "
+	vim.g.maplocalleader = " "
+
 	vim.api.nvim_create_autocmd({ "BufEnter" }, {
 		callback = function(args)
 			local setups = {}
@@ -89,6 +92,25 @@ M.setup = function(opts)
 			end
 		end,
 	})
+
+	-- Mini Deps
+
+	local deps_path = vim.fn.stdpath("data") .. "/site/pack/deps/start/mini.deps"
+
+	if not vim.loop.fs_stat(deps_path) then
+		vim.cmd('echo "Installing `mini.deps`" | redraw')
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/nvim-mini/mini.deps",
+			deps_path,
+		})
+		vim.cmd("packadd mini.deps | helptags ALL")
+		vim.cmd('echo "Installed `mini.deps`" | redraw')
+	end
+
+	require("mini.deps").setup()
 
 	-- Treesitter
 
