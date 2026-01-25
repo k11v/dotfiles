116090b630581f3a2a5dea1dd990ebd2886e420e
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2026-01-25 15:52:15 +0300

    nvim: configure timeout, signcolumn, hidden, LSP progress, search

--- nvim/.config/init.lua
+++ nvim/.config/init.lua
@@ -37,6 +37,14 @@ vim.g.loaded_ruby_provider = 1
 -- You can wrap manually with "ggn".
 vim.opt.wrapscan = false
 
+vim.opt.timeout = false
+
+vim.opt.signcolumn = "yes"
+
+-- Disable 'hidden' to reduce buffer list pollution with modified buffers.
+-- The default is on in Neovim and off in Vim.
+vim.opt.hidden = false
+
 -- Other.
 
 -- Augroup.
@@ -72,7 +80,7 @@ vim.lsp.config("gopls", {
 			buildFlags = { "-tags=dev,integration" }, -- temporary
 			directoryFilters = { "-.git" },
 			gofumpt = true,
-			renameMovesSubpackages = true,
+			renameMovesSubpackages = true, -- requires gopls >= 0.21.0
 			semanticTokens = true,
 			staticcheck = true,
 			usePlaceholders = true,
@@ -92,3 +100,85 @@ vim.lsp.enable("gopls")
 
 -- Go LSP formatting.
 vim.cmd([[autocmd g BufWritePre *.go lua vim.lsp.buf.format({})]])
+
+-- LSP progress.
+local lsp_progress_func
+do
+	local progress_from_key = {}
+
+	lsp_progress_func = function(args)
+		local buffer_id = args.buf
+		local client_id = args.data.client_id
+		local token = args.data.params.token
+		local value = args.data.params.value
+
+		-- Get client name.
+		local client = vim.lsp.get_client_by_id(client_id)
+		if client == nil then
+			return
+		end
+		local client_name = client.name
+
+		-- Get progress key.
+		local progress_key = string.format("%s:%s:%s", buffer_id, client_id, token)
+		local progress_value = progress_from_key[progress_key]
+
+		-- Get and set notification parameters.
+		if value.kind == "begin" then
+			if progress_value ~= nil then
+				return
+			end
+			progress_value = {}
+			progress_value.name = client.name or ""
+			progress_value.title = value.title or ""
+			progress_value.message = value.message or ""
+			progress_value.percentage = 0
+			progress_from_key[progress_key] = progress_value
+		elseif value.kind == "report" then
+			if progress_value == nil then
+				return
+			end
+			progress_value.message = value.message or ""
+			progress_value.percentage = value.percentage or progress_value.percentage
+			progress_from_key[progress_key] = progress_value
+		elseif value.kind == "end" then
+			if progress_value == nil then
+				return
+			end
+			progress_value.message = value.message or ""
+			progress_value.percentage = 100
+			progress_from_key[progress_key] = progress_value
+		else
+			return
+		end
+
+		-- Get notification.
+		local notification = string.format(
+			"%s: %s%s%s%s(%s%%)",
+			progress_value.name,
+			progress_value.title,
+			progress_value.title == "" and "" or " ",
+			progress_value.message,
+			progress_value.message == "" and "" or " ",
+			progress_value.percentage
+		)
+
+		-- Send notification.
+		vim.notify(notification)
+	end
+end
+
+vim.api.nvim_create_autocmd("LspProgress", { group = vim.g.augroup, callback = lsp_progress_func })
+
+-- Search
+vim.api.nvim_create_user_command("Search", function(opts)
+  local s = opts.args
+
+  -- Escape regex-special chars, then turn underscores into "." wildcards.
+  local escaped = vim.fn.escape(s, [[\.^$~[]*+?(){}|]])
+  local pattern = escaped:gsub("_", ".")
+
+  -- Set search register and jump to next match.
+  vim.fn.setreg("/", pattern)
+  vim.cmd("normal! n")
+end, { nargs = 1 })
