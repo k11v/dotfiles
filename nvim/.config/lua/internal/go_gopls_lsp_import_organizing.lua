5f4d476d11474f9fc67d32ad434d74e26d13d9ae
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2026-02-02 01:23:10 +0300

    nvim: finalize code_action impl with edit and without command

--- nvim/.config/lua/internal/go_gopls_lsp_import_organizing.lua
+++ nvim/.config/lua/internal/go_gopls_lsp_import_organizing.lua
@@ -1,5 +1,15 @@
 local mod = "go_gopls_lsp_import_organizing"
 
+-- As of 2026-02-01 note that Client:request flushes didChange notifications only for the provided buf.
+-- It doesn't flush changes for other buffers, it looks like it could lead to codeAction faults.
+
+-- Good enough Vim pos to LSP pos conversion.
+-- Assumes that cursor points at an actual character or an empty line.
+-- Doesn't assume that cursor points at non-existing character, e.g. in Visual block mode.
+_G.lsp_pos_from_vim_pos = function(pos)
+	return { line = pos[1] - 1, character = pos[2] }
+end
+
 -- get_buf_cursor gets buf cursor position for any valid buffer, including hidden.
 _G.get_buf_cursor = function(buf)
 	buf = buf ~= 0 and buf or vim.api.nvim_get_current_buf()
@@ -26,227 +36,179 @@ _G.get_buf_cursor = function(buf)
 	return { 1, 0 }
 end
 
-_G.code_action = function()
-	-- TODO: Support as a parameter.
-	local buf = vim.api.nvim_get_current_buf()
+_G.code_action = function(client, buf, code_action_context)
+	vim.validate('client', client, 'table', true)
+	vim.validate('buf', buf, 'number')
+	vim.validate('code_action_context', code_action_context, 'table')
 
-	-- TODO: Support as a parameter.
-	-- TODO: Support multiple.
-	local client = vim.lsp.get_clients({ bufnr = buf, method = "textDocument/codeAction" })[1]
+	buf = buf ~= 0 and buf or vim.api.nvim_get_current_buf()
+	if not vim.api.nvim_buf_is_valid(buf) then
+		return
+	end
+
+	client = client or vim.lsp.get_clients({ bufnr = buf, method = "textDocument/codeAction" })[1]
 	if client == nil then
 		vim.notify("code action missing client", vim.log.levels.INFO)
 		return
 	end
 
-	-- REQUEST
+	-- Get code action.
 
-	-- As of 2026-02-01 note that Client:request flushes didChange notifications only for the provided buf.
-	-- It doesn't flush changes for other buffers, it looks like it could lead to codeAction faults.
-
-	local codeActionParams = nil
-	local codeActionResult = nil
-	local codeActionCancel = nil
+	local code_action_params = nil
+	local code_action_result = nil
+	local code_action_cancel = nil
 
 	do
-		local pos = get_buf_cursor(buf)
-		local range_params = vim.lsp.util.make_given_range_params(pos, pos, buf, client.offset_encoding)
-		codeActionParams = {
-			textDocument = range_params.textDocument,
-			range = range_params.range,
-			context = {
-				diagnostics = {},
-				only = nil,
-				triggerKind = nil,
-			},
+		local vim_pos = get_buf_cursor(buf)
+		local lsp_pos = lsp_pos_from_vim_pos(vim_pos)
+		code_action_params = {
+			textDocument = vim.lsp.util.make_text_document_params(buf),
+			range = { start = lsp_pos, ["end"] = lsp_pos },
+			context = code_action_context,
 		}
 	end
 
 	do
 		local ok, id = client:request(
 			"textDocument/codeAction",
-			codeActionParams,
-			function(err, ok) codeActionResult = { ok = ok, err = err } end,
+			code_action_params,
+			function(err, ok) code_action_result = { ok = ok, err = err } end,
 			buf
 		)
 		if ok then
-			codeActionCancel = function() client:cancel_request(id) end
+			code_action_cancel = function() client:cancel_request(id) end
 		else
-			codeActionResult = { err = {} }
+			code_action_result = { err = {} }
 		end
 	end
 
 	do
-		local ok, _ = vim.wait(1000, function() return codeActionResult ~= nil end, 10)
+		local ok, _ = vim.wait(1000, function() return code_action_result ~= nil end, 10)
 		if not ok then
-			codeActionCancel()
+			code_action_cancel()
 			vim.notify("code action timeout", vim.log.levels.INFO)
 			return
 		end
 	end
 
-	local codeActionOkResult = nil
+	local code_action_ok_result = nil
 
 	do
-		local err = codeActionResult.err
+		local err = code_action_result.err
 		if err ~= nil then
 			vim.notify(string.format("code action error: %s", vim.inspect(err)), vim.log.levels.INFO)
 			return
 		end
-		codeActionOkResult = codeActionResult.ok
+		code_action_ok_result = code_action_result.ok
 	end
 
-	-- REQUEST
-
-	local resolveParams = nil
-	local resolveResult = nil
-	local resolveCancel = nil
+	local code_action = nil
 
 	do
-		resolveParams = codeActionOkResult[1]
-	end
-
-	do
-		local ok, id = client:request(
-			"codeAction/resolve",
-			resolveParams,
-			function(err, ok) resolveResult = { ok = ok, err = err } end,
-			buf
-		)
-		if ok then
-			resolveCancel = function() client:cancel_request(id) end
-		else
-			resolveResult = { err = {} }
-		end
-	end
-
-	do
-		local ok, _ = vim.wait(1000, function() return resolveResult ~= nil end, 10)
-		if not ok then
-			resolveCancel()
-			vim.notify("code action resolve timeout", vim.log.levels.INFO)
+		if code_action_ok_result == nil or #code_action_ok_result == 0 then
+			vim.notify("code action not found", vim.log.levels.INFO)
 			return
 		end
-	end
 
-	local resolveOkResult = nil
-
-	do
-		local err = resolveResult.err
-		if err ~= nil then
-			vim.notify(string.format("code action resolve error: %s", vim.inspect(err)), vim.log.levels.INFO)
+		if #code_action_ok_result > 1 then
+			vim.notify("multiple code actions", vim.log.levels.INFO)
 			return
 		end
-		resolveOkResult = resolveResult.ok
+
+		code_action = code_action_ok_result[1]
 	end
 
-	return { codeActionOkResult = codeActionOkResult, resolveOkResult = resolveOkResult }
+	-- Resolve code action.
 
-	-- if not (action.edit and action.command) and client:supports_method('codeAction/resolve') then
+	-- LSP spec is not clear about when code action should be resolved.
+	-- We could interspect code action and resolve if edit or command
+	-- is missing but for now resolving every time is good enough.
+	if client:supports_method('codeAction/resolve') then
+		local resolve_params = nil
+		local resolve_result = nil
+		local resolve_cancel = nil
 
-	-- local function apply_action(action, client, ctx)
-	--   if action.edit then
-	--     util.apply_workspace_edit(action.edit, client.offset_encoding)
-	--   end
-	--   local a_cmd = action.command
-	--   if a_cmd then
-	--     local command = type(a_cmd) == 'table' and a_cmd or action
-	--     --- @cast command lsp.Command
-	--     client:exec_cmd(command, ctx)
-	--   end
-	-- end
+		do
+			resolve_params = code_action
+		end
 
+		do
+			local ok, id = client:request(
+				"codeAction/resolve",
+				resolve_params,
+				function(err, ok) resolve_result = { ok = ok, err = err } end,
+				buf
+			)
+			if ok then
+				resolve_cancel = function() client:cancel_request(id) end
+			else
+				resolve_result = { err = {} }
+			end
+		end
 
-	-- -- Client:request_sync({method}, {params}, {timeout_ms}, {bufnr})
-	-- client:request_sync()
+		do
+			local ok, _ = vim.wait(1000, function() return resolve_result ~= nil end, 10)
+			if not ok then
+				resolve_cancel()
+				vim.notify("code action resolve timeout", vim.log.levels.INFO)
+				return
+			end
+		end
 
-	-- SEND ASYNC TO ALL
-	-- Client:request({method}, {params}, {handler}, {bufnr})
-	--
-	-- client:request() -- textDocument/codeAction
-	--
+		local resolve_ok_result = nil
 
-	-- SEND ASYNC TO THOSE THAT NEED RESOLVING, SAME CALLBACK
-	-- Client:request({method}, {params}, {handler}, {bufnr})
-	--
-	-- client:request() -- codeAction/resolve
-	--
-	-- local client = assert(lsp.get_client_by_id(choice.ctx.client_id))
-	-- local action = choice.action
-	-- local bufnr = assert(choice.ctx.bufnr, 'Must have buffer number')
-	--
-	-- -- Only code actions are resolved, so if we have a command, just apply it.
-	-- if type(action.title) == 'string' and type(action.command) == 'string' then
-	--   apply_action(action, client, choice.ctx)
-	--   return
-	-- end
-	--
-	-- if action.disabled then
-	--   vim.notify(action.disabled.reason, vim.log.levels.ERROR)
-	--   return
-	-- end
-	--
-	-- if not (action.edit and action.command) and client:supports_method('codeAction/resolve') then
-	--   client:request('codeAction/resolve', action, function(err, resolved_action)
-	--     if err then
-	--       -- If resolve fails, try to apply the edit/command from the original code action.
-	--       if action.edit or action.command then
-	--         apply_action(action, client, choice.ctx)
-	--       else
-	--         vim.notify(err.code .. ': ' .. err.message, vim.log.levels.ERROR)
-	--       end
-	--     else
-	--       apply_action(resolved_action, client, choice.ctx)
-	--     end
-	--   end, bufnr)
-	-- else
-	--   apply_action(action, client, choice.ctx)
-	-- end
+		do
+			local err = resolve_result.err
+			if err ~= nil then
+				vim.notify(string.format("code action resolve error: %s", vim.inspect(err)), vim.log.levels.INFO)
+				return
+			end
+			resolve_ok_result = resolve_result.ok
+		end
 
-	-- NOW WAIT FOR ALL, INCLUDING codeAction/resolve
-	-- local wait_result, reason = vim.wait(timeout_ms or 1000, function()
-	-- 	return request_result ~= nil
-	-- end, 10)
-	-- if not wait_result then
-	-- 	if request_id then
-	-- 		self:cancel_request(request_id)
-	-- 	end
-	-- 	return nil, wait_result_reason[reason]
-	-- end
-	-- return request_result
+		do
+			code_action = resolve_ok_result
+		end
+	end
 
-	-- APPLY
-	-- https://github.com/fnune/codeactions-on-save.nvim/blob/main/lua/codeactions-on-save/main.lua#L15
-	-- ---@param action lsp.Command|lsp.CodeAction
-	-- ---@param client vim.lsp.Client
-	-- ---@param ctx lsp.HandlerContext
-	-- local function apply_action(action, client, ctx)
-	--   if action.edit then
-	--     util.apply_workspace_edit(action.edit, client.offset_encoding)
-	--   end
-	--   local a_cmd = action.command
-	--   if a_cmd then
-	--     local command = type(a_cmd) == 'table' and a_cmd or action
-	--     --- @cast command lsp.Command
-	--     client:exec_cmd(command, ctx)
-	--   end
-	-- end
+	-- Do code action.
+
+	if code_action.edit then
+		vim.lsp.util.apply_workspace_edit(code_action.edit, client.offset_encoding)
+	end
+
+	if code_action.command then
+		vim.notify("code action command not supported", vim.log.levels.INFO)
+		-- Code below won't work if command is executed on the client (handler never gets called).
+		--
+		-- local done = false
+		-- client:exec_cmd(
+		-- 	code_action.command,
+		-- 	{ bufnr = buf }, -- TODO: maybe something better should be inserted here.
+		-- 	function(err, ok) done = true end
+		-- )
+		--
+		-- local ok, _ = vim.wait(1000, function() return done or client:is_stopped() end, 10)
+		-- if not ok then
+		-- 	vim.notify("code action execute command timeout", vim.log.levels.INFO)
+		-- 	return
+		-- end
+	end
 end
 
 vim.api.nvim_create_autocmd("BufWritePre", {
 	group = vim.api.nvim_create_augroup(mod, {}),
 	pattern = "*.go",
 	callback = function(args)
-		vim.lsp.buf.code_action({
-			context = {
-				-- diagnostics = {}, -- here we don't need any diagnostics
-				only = { "source.organizeImports" },
-				-- triggerKind = 2, -- automatic
-			},
-			filter = function(_, client_id) return vim.lsp.get_client_by_id(client_id).name == "gopls" end,
-			apply = true,
-			-- range = { ... }, -- default is to respect selection but probably action ignores it
-			-- timeout_ms = 1000, -- didn't find in the docs
-			-- bufnr = args.buf, -- didn't find in the docs
-		})
+		client = client or vim.lsp.get_clients({
+			bufnr = buf,
+			name = "gopls",
+			method = "textDocument/codeAction",
+		})[1]
+		if client == nil then
+			return
+		end
+
+		code_action(client, args.buf, { diagnostics = {}, only = { "source.organizeImports" } })
 	end,
 })
