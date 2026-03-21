790d223a0f9bfe1ba74cbe7fd4ce4016530282cd
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2026-03-22 00:37:32 +0300

    Add nvim Restart cmd

---
+++ nvim/.config/nvim/lua/internal/restart.lua
@@ -0,0 +1,10 @@
+local mod = "restart"
+
+local function restart(expr)
+	vim.cmd([[source $MYVIMRC]])
+end
+
+pcall(vim.api.nvim_del_user_command, "Restart")
+vim.api.nvim_create_user_command("Restart", function(o)
+  restart(o.args)
+end, {})
