511e49865df5e717595dd55436d00f786d3d786b
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2026-03-21 18:35:28 +0300

    Configure zsh history

--- zsh/.integration/zsh/old.zsh
+++ zsh/.integration/zsh/old.zsh
@@ -691,16 +691,17 @@ lspypi() {
 # setopt GLOB_COMPLETE           # Generate completions with globs
 # unsetopt LIST_BEEP             # Suppress beep on an ambiguous completion
 #
-# # History
-#
-# setopt EXTENDED_HISTORY        # Save each command's timestamp in history
-# unsetopt HIST_BEEP             # Suppress beep on non-existent history access
-# setopt HIST_EXPIRE_DUPS_FIRST  # Expire duplicate events from history first
-# setopt HIST_IGNORE_DUPS        # Do not record a just recorded event again
-# setopt HIST_IGNORE_SPACE       # Do not record an event starting with a space
-# setopt HIST_SAVE_NO_DUPS       # Do not save duplicate events in history
-# setopt SHARE_HISTORY           # Share history between all sessions
-#
+
+# History
+
+setopt EXTENDED_HISTORY        # Save each command's timestamp in history
+unsetopt HIST_BEEP             # Suppress beep on non-existent history access
+setopt HIST_EXPIRE_DUPS_FIRST  # Expire duplicate events from history first
+setopt HIST_IGNORE_DUPS        # Do not record a just recorded event again
+setopt HIST_IGNORE_SPACE       # Do not record an event starting with a space
+setopt HIST_SAVE_NO_DUPS       # Do not save duplicate events in history
+setopt SHARE_HISTORY           # Share history between all sessions
+
 # # Input/output
 #
 # unsetopt FLOW_CONTROL          # Make '^S' and '^Q' key bindings available
