403d59ee9efa47ab0d5655fe4e1fcdb13fb2e55b
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2026-04-04 17:48:34 +0300

    Migrate LspProgress to nvim's built-in progress

--- nvim/.config/nvim/lua/internal/lsp_progress.lua
+++ nvim/.config/nvim/lua/internal/lsp_progress.lua
@@ -1,71 +1,61 @@
 local mod = "internal.lsp_progress"
 
+local lsp_progress_kind_begin = "begin"
+local lsp_progress_kind_report = "report"
+local lsp_progress_kind_end = "end"
+
+local progress_kind_progress = "progress"
+
+local progress_status_success = "success"
+local progress_status_running = "running"
+local progress_status_failed = "failed"
+local progress_status_cancel = "cancel"
+
 local group = vim.api.nvim_create_augroup(mod, {})
 
-local lsp_progress
+local lsp_progress_callback
 do
 	local progress_from_key = {}
 
-	lsp_progress = function(args)
-		local buffer_id = args.buf
-		local client_id = args.data.client_id
-		local token = args.data.params.token
-		local value = args.data.params.value
+	lsp_progress_callback = function(args)
+		local lsp_client_id = args.data.client_id
 
-		-- Get client name.
-		local client = vim.lsp.get_client_by_id(client_id)
-		if client == nil then
+		local lsp_client_token = args.data.params.token
+
+		---@type { kind?: string, title?: string, message?: string, percentage?: integer }
+		local lsp_progress = args.data.params.value
+
+		local lsp_client = vim.lsp.get_client_by_id(lsp_client_id)
+		if lsp_client == nil then
 			return
 		end
-		local client_name = client.name
 
-		-- Get progress key.
-		local progress_key = string.format("%s:%s:%s", buffer_id, client_id, token)
-		local progress_value = progress_from_key[progress_key]
+		local progress_key = string.format("%s:%s", lsp_client_id, lsp_client_token)
 
-		-- Get and set notification parameters.
-		if value.kind == "begin" then
-			if progress_value ~= nil then
-				return
-			end
-			progress_value = {}
-			progress_value.name = client.name or ""
-			progress_value.title = value.title or ""
-			progress_value.message = value.message or ""
-			progress_value.percentage = 0
-			progress_from_key[progress_key] = progress_value
-		elseif value.kind == "report" then
-			if progress_value == nil then
-				return
-			end
-			progress_value.message = value.message or ""
-			progress_value.percentage = value.percentage or progress_value.percentage
-			progress_from_key[progress_key] = progress_value
-		elseif value.kind == "end" then
-			if progress_value == nil then
-				return
-			end
-			progress_value.message = value.message or ""
-			progress_value.percentage = 100
-			progress_from_key[progress_key] = progress_value
+		---@type { id?: string|integer, kind?: string, percent?: integer, source?: string, status?: string, title?: string }
+		local progress = vim.deepcopy(progress_from_key[progress_key]) or {}
+
+		if lsp_progress.kind == lsp_progress_kind_begin then
+			progress.kind = progress_kind_progress
+			progress.percent = 0
+			progress.source = mod
+			progress.status = progress_status_running
+			progress.title = (lsp_client.name and lsp_client.name or "client " .. tostring(lsp_client.id))
+				.. (lsp_progress.title and ": " .. lsp_progress.title or "")
+		elseif lsp_progress.kind == lsp_progress_kind_report then
+			progress.percent = lsp_progress.percentage
+		elseif lsp_progress.kind == lsp_progress_kind_end then
+			progress.percent = 100
+			progress.status = progress_status_success
 		else
 			return
 		end
 
-		-- Get notification.
-		local notification = string.format(
-			"%s: %s%s%s%s(%s%%)",
-			progress_value.name,
-			progress_value.title,
-			progress_value.title == "" and "" or " ",
-			progress_value.message,
-			progress_value.message == "" and "" or " ",
-			progress_value.percentage
-		)
+		local message = lsp_progress.message or ""
 
-		-- Send notification.
-		vim.notify(notification)
+		progress.id = vim.api.nvim_echo({ { message } }, true, progress)
+		progress_from_key[progress_key] = progress
 	end
 end
 
-vim.api.nvim_create_autocmd("LspProgress", { group = group, callback = lsp_progress })
+vim.api.nvim_create_autocmd("LspProgress", { group = group, callback = lsp_progress_callback })
