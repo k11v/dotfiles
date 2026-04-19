368c3a07e6c82a2e259520b41cb0e17f891d4d94
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2026-04-20 02:09:33 +0300

    Highlight cursor in terminal normal mode in nvim

--- nvim/.config/nvim/lua/internal/term.lua
+++ nvim/.config/nvim/lua/internal/term.lua
@@ -2,3 +2,18 @@ local mod = "term"
 
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 vim.keymap.set("t", [[<C-\><Esc>]], "<Esc>", { desc = "Send Esc to terminal" })
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+	pattern = "*:nt",
+	callback = function()
+		vim.wo.cursorline = true
+		vim.wo.cursorcolumn = true
+	end,
+})
+vim.api.nvim_create_autocmd("ModeChanged", {
+	pattern = "nt:*",
+	callback = function()
+		vim.wo.cursorline = false
+		vim.wo.cursorcolumn = false
+	end,
+})
