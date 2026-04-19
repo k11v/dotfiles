8773c31eda89ca0e752419e77a9f8295655ebaf6
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2026-04-20 01:17:00 +0300

    Add <C-\><Esc> to send Esc to terminal to nvim

--- nvim/.config/nvim/lua/internal/term.lua
+++ nvim/.config/nvim/lua/internal/term.lua
@@ -1,4 +1,4 @@
 local mod = "term"
 
--- <Esc> exits terminal mode. Use <C-\><Esc> to send a raw Esc to the terminal.
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+vim.keymap.set("t", [[<C-\><Esc>]], "<Esc>", { desc = "Send Esc to terminal" })
