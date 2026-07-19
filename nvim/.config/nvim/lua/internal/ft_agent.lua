b5add65abaff8b51e8df853907823b65cadd950f
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2026-07-20 00:45:25 +0300

    Fix x:// scheme to be xx://
    Due to min scheme name length of 2.

--- nvim/.config/nvim/lua/internal/ft_agent.lua
+++ nvim/.config/nvim/lua/internal/ft_agent.lua
@@ -3,10 +3,9 @@ local group = vim.api.nvim_create_augroup(mod, {})
 
 vim.api.nvim_create_autocmd("BufReadCmd", {
 	group = group,
-	pattern = { "x://agent", "x://agent/*" },
+	pattern = { "xx://agent", "xx://agent/*" },
 	callback = function(args)
 		local name = vim.api.nvim_buf_get_name(args.buf)
-		name = name:match("x://.*")
 		vim.fn.jobstart("claude --allow-dangerously-skip-permissions", { term = true })
 		vim.api.nvim_buf_set_name(args.buf, name)
 	end,
--- nvim/.config/nvim/lua/internal/ft_term.lua
+++ nvim/.config/nvim/lua/internal/ft_term.lua
@@ -3,10 +3,9 @@ local group = vim.api.nvim_create_augroup(mod, {})
 
 vim.api.nvim_create_autocmd("BufReadCmd", {
 	group = group,
-	pattern = { "x://term", "x://term/*" },
+	pattern = { "xx://term", "xx://term/*" },
 	callback = function(args)
 		local name = vim.api.nvim_buf_get_name(args.buf)
-		name = name:match("x://.*")
 		vim.fn.jobstart(vim.o.shell, { term = true })
 		vim.api.nvim_buf_set_name(args.buf, name)
 	end,
