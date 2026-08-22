09ce4904a3702b43da3033774556cdcc7a141143
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2026-08-22 10:41:25 +0300

    Extract titleof program from Zsh functions

--- zsh/zsh/old.zsh
+++ zsh/zsh/old.zsh
@@ -331,11 +331,6 @@ recompinit() {
     compinit -d "$ZCOMPDUMP"
 }
 
-# Get website's title
-titleof() {
-    curl -sSL -o - -- "$1" | perl -l -0777 -n -e 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si'
-}
-
 printcolors() {
     printf "%s %s%s%s %s%s%s %s%s%s\n" \
         " " ""                "NORMAL " ""             "$(tput bold)"                "BOLD NORMAL " "$(tput sgr0)" ""                "BACKGROUND NORMAL " ""             \
