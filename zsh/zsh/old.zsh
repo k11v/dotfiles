a5d2c05a5cb2416eaeb7de8ecad3ae7679fbac16
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2026-08-22 13:28:40 +0300

    Extract prompt.zsh

--- zsh/zsh/old.zsh
+++ zsh/zsh/old.zsh
@@ -1,68 +1,6 @@
 export PATH="/opt/homebrew/opt/libpq/bin:$PATH" # brew install libpq
-export PATH="$PATH:/opt/homebrew/opt/git/bin" # use newer Git from Homebrew
-export PATH="$PATH:$HOME/.local/dist/nvim/bin"
-
-# Prompt
-
-PROMPT_EOL_MARK=""
-PS2="%B…%b "
-function _prompt_precmd() {
-    local exit_status="$?" git_ref git_commit git_tag is_git_worktree
-    local username hostname branch context venv workdir symbol
-    
-    workdir="%B%F{cyan}%~%f%b"
-    symbol="%B%F{green}❯%f%b"
-
-    # is_git_worktree is "true", "false", or "" (when not in a Git repository).
-    is_git_worktree="$(git rev-parse --is-inside-work-tree 2> /dev/null)"
-
-    if [[ "$is_git_worktree" == "true" ]]; then
-        git_ref="$(git symbolic-ref --quiet HEAD 2> /dev/null)"
-        [[ "$?" -eq 1 ]] && git_commit="$(git rev-parse --short HEAD 2> /dev/null)"
-        [[ -n "$git_commit" ]] && git_tag="$(git describe --exact-match --tags "$git_commit" 2> /dev/null)"
-
-        if [[ -d ".git" ]]; then
-            workdir="%B%F{cyan}${PWD:t}%f%b"
-        fi
-
-        if [[ -n "$git_ref" ]]; then
-            branch=" on %B%F{magenta}${git_ref#refs/heads/}%f%b"
-        elif [[ -n "$git_tag" ]]; then
-            branch=" on %B%F{magenta}HEAD%f%b %B%F{green}($git_tag)%b%f"
-        elif [[ -n "$git_commit" ]]; then
-            branch=" on %B%F{magenta}HEAD%f%b %B%F{green}($git_commit)%b%f"
-        fi
-    elif [[ "$is_git_worktree" == "false" ]]; then
-        branch=" on %B%F{magenta}HEADLESS%f%b"
-    fi
-
-    if [[ -n "${SSH_CONNECTION-}${SSH_CLIENT-}${SSH_TTY-}" ]]; then
-        username="%B%F{yellow}%n%f%b in "
-        hostname="%B%F{green}%m%f%b in "
-    fi
-
-    if [[ "${EUID-}" -eq 0 ]]; then
-        username="%B%F{red}%n%f%b in "
-    fi
-
-    if [[ -n "${X_CONTEXT-}" ]]; then
-        context=" via %B%F{blue}${X_CONTEXT}%f%b"
-    fi
-
-    if [[ -n "${VIRTUAL_ENV-}" ]]; then
-        venv=" via %B%F{yellow}${VIRTUAL_ENV:t}%f%b"
-    elif [[ -n "${PYENV_VERSION-}" ]]; then
-        venv=" via %B%F{yellow}${PYENV_VERSION}%f%b"
-    fi
-
-    if [[ "$exit_status" -ne 0 ]]; then
-        symbol="%B%F{red}❯%f%b"
-    fi
-
-    PROMPT="$username$hostname$workdir$branch$context$venv $symbol "
-}
-autoload -Uz add-zsh-hook
-add-zsh-hook precmd _prompt_precmd
+export PATH="$PATH:/opt/homebrew/opt/git/bin" # brew install git
+export PATH="$PATH:$HOME/.local/dist/nvim/bin" # make-nvim
 
 # Aliases
 
@@ -127,17 +65,3 @@ alias cdf='cd "$(finder-pwd)"'  # finder-pwd is user-defined
 function bat() { command bat --paging=never "$@" }
 function grep() { command grep --color=auto "$@" }
 function ls() { command ls --color=auto "$@" }
-
-#
-# From older zshrc
-#
-
-# Zsh
-
-# PROMPT_STYLE="regular"                      # User-defined
-
-# My utils
-
-# export WORKSPACES="$HOME/Workspaces"
-# export NOTES="$HOME/Notes"
-# export DOTFILES_ENVIRONMENT_LOADED=1
